8014d728fbb757a53977689e84977aee1e6416e3
C5356976 <anu.jose01@sap.com>
2023-08-16 09:55:17 +0530

    first commit

---
+++ src/main/java/com/example/springcf7/SpringCf7Application.java
@@ -0,0 +1,14 @@
+package com.example.springcf7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCf7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCf7Application.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/example/springcf7/SpringCf7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcf7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCf7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
